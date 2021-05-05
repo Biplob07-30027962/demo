04c56db3f8b25ba3b4d49c49040e94fbb8328c3d
Biplob Paudel <Biplob07-30027962>
2021-05-05 12:53:37 +1200

    Signed-off-by: Biplob Paudel <Biplob07-30027962>

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p1>myname is biplob paudel</p1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
